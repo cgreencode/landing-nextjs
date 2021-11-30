be07b8d064089b71adb265619574385f0f16ea0e
anproagile <anproagile@gmail.com>
2021-11-30 02:34:26 +0200

    Add about page

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout/Layout";
+import Link from 'next/link'
+
+function About() {
+    return (
+        <Layout>
+            <div>This page is now blank. I will fill it in future</div>
+            <Link href="/">Main page</Link>
+        </Layout>
+    );
+}
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import Layout from "../components/Layout/Layout";
+import Link from 'next/link'
 
 function App() {
-    return (<Layout><div>Welcome to my page!</div></Layout>);
+    return (
+        <Layout>
+            <div>Hello this is my main pages. If you want to know about me more pleaze preceed link below</div>
+            <Link href="/about">About me!</Link>
+        </Layout>
+    );
 }
 
 export default App;
