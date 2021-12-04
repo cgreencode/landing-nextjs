b97af3f4924e66ec7b0d6903d22d81d806903336
anproagile <anproagile@gmail.com>
2021-12-04 15:42:39 +0200

    Fix for path

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
     basePath: process.env.PRODUCTION === 'true' ? '/landing-nextjs' : '',
     images: {
         loader: 'imgix',
-        path: process.env.PRODUCTION === 'true' ? '/landing-nextjs' : '',
+        path: '',
     },
 }
