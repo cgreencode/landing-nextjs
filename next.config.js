245d1d0bf6e6a24f04cb1ff20735b9773ec267f3
anproagile <anproagile@gmail.com>
2021-12-05 02:03:29 +0200

    Fix for static images

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-const withOptimizedImages = require('next-optimized-images');
-
 module.exports = {
     basePath: process.env.PRODUCTION === 'true' ? '/landing-nextjs' : '',
-    images: {
-        domains: ['openweathermap.org'],
-    },
-    ...withOptimizedImages({
-        handleImages: ['jpeg', 'png', 'svg'],
-    })
 }
